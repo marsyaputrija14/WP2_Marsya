e45fbca14385ef8a6096f7d79e3e594c9dc79e17
marsyaputrija14 <marsya230399@gmail.com>
2022-09-13 10:42:23 +0700

    menambahkan welcome.php,contoh1.php,index.html

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo " Nama saya Marsya Putrija saya tinggal di daerah bekasi olahraga yang saya sukai adalah voly";
+    }
+}
